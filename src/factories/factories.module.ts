d5419e1b42c9ef9908fa29ed90171f1e93fac3a4
Nikita <n.staselovich@codemaker.click>
2024-10-10 10:10:45 +0300

    Factories endpoint

--- src/factories/factories.module.ts
+++ src/factories/factories.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { FactoriesService } from './factories.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MessagesModule } from '../messages/messages.module';
+import { FactoriesController } from './factories.controller';
 
 @Module({
   imports: [PrismaModule, MessagesModule],
   providers: [FactoriesService],
+  controllers: [FactoriesController],
 })
 export class FactoriesModule {}
